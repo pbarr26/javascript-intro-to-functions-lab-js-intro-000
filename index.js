74676cedaa1fa93795c696a564be0f14558c7847
cerebral-map-4184 <patriciagbarrett@gmail.com>
2018-12-31 01:23:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,16 +16,19 @@ console.log("hello".toLowerCase());
 
 function sayHiToGrandma(string) {
 if (string.toLowerCase() === string) {
-    {   return 'I can\'t hear you!' ;
+    {
+      return 'I can\'t hear you!' ;
 
 
 }  else if (string.toUpperCase() === string)
-    {  return 'YES INDEED!';
+    {
+      return 'YES INDEED!';
 
  }
 
 } else ("I love you, Grandma."===string) {
-      {   return "I love you, too." ;
+      {
+        return "I love you, too." ;
     }
 
 
