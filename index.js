ffa4b582b80811aec32738b3bd43f74ae1a44cd6
cerebral-map-4184 <patriciagbarrett@gmail.com>
2018-12-31 00:40:45 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+return string.toUpperCase()
+}
+
+function whisper(string) {
+return string.toLowerCase()
+}
+
+function logShout(string) {
+console.log("hello".toUpperCase());
+}
+
+function logWhisper(string) {
+console.log("hello".toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+if (string.toLowerCase() === string) {
+       return 'I can\'t hear you!' ;
+      }
+ }
+
+function sayHiToGrandma(string) {
+if (string.toUpperCase() === string) {
+       return 'YES INDEED!';
+      }
+ }
+
+function sayHiToGrandma(string) {
+  if ("I love you, Grandma."===string) {
+         return "I love you, too." ;
+        }
+   }
