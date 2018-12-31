dfa60c103275f4bb6c752dcac2c9a09bb0d70bcf
cerebral-map-4184 <patriciagbarrett@gmail.com>
2018-12-31 01:18:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ if (string.toLowerCase() === string) {
     {   return 'I can\'t hear you!' ;
 
 
-}  else if (string.toUpperCase() === string) 
+}  else if (string.toUpperCase() === string)
     {   return 'YES INDEED!';
 
  }
